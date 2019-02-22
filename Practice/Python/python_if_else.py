b7587e079199bfd90e823c92cd8ed896bbc2655a
Shishir Kumar <45385122+shishir1990@users.noreply.github.com>
2019-02-22 12:21:26 +0530

    Add files via upload
    Added Python

---
+++ Practice/Python/python_if_else.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+N = int(input())
+
+if(N%2 == 0):
+    if (N in range(2,5)):
+        print("Not Weird")
+    elif(N in range(6,21)):
+        print("Weird")
+    elif(N > 20):
+        print("Not Weird")
+else:
+    print("Weird")
---
+++ Practice/Python/rotate_left.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    l = a[d:]+a[:d]
+    for item in l:
+        print(item, end=" ")
